ed75ad71eb88abdb9fa8df73c0dff670b6a54fac
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-08 10:36:45 +0700

    Add files via upload

---
+++ tugas5_2.php
@@ -0,0 +1,32 @@
+<?php
+	$input1 = ["Ayo ", "Bersama"];
+	$input2 = ["Belajar ", " Niomic"];
+	
+
+	echo "<h2>Operator String</h2>";
+	echo "<table border=1>
+			<tr>
+				<td>Input 1</td>
+				<td>Input 2</td>
+				<td>Hasil</td>
+			</tr>
+
+			<tr>
+				<td>Ayo</td>
+				<td>Belajar</td>
+				<td>".$input1[0].$input2[0]."</td>
+			</tr>
+
+			<tr>
+				<td>Bersama</td>
+				<td>Niomic</td>
+				<td>".$input1[1].$input2[1]."</td>
+			</tr>
+
+			</table>";
+
+
+	
+
+	
+?>
